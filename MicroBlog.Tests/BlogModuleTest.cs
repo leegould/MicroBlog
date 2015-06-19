1f8b812e3e515f6582127033dfb841a3622f5947
Lee <gould.lee@googlemail.com>
2015-06-19 08:38:03 +0100

    Setting up project

---
+++ MicroBlog.Tests/BlogModuleTest.cs
@@ -0,0 +1,23 @@
+﻿using Nancy;
+using Nancy.Testing;
+using Xunit;
+
+namespace MicroBlog.Tests
+{
+    public class BlogModuleTest
+    {
+        [Fact]
+        public void Should_Return_Blog_When_Queried_With_Id()
+        {
+            var bootstrapper = new DefaultNancyBootstrapper();
+            var browser = new Browser(bootstrapper);
+
+            var result = browser.Get("/1", with =>
+            {
+                with.HttpRequest();
+            });
+
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+        }
+    }
+}
---
+++ MicroBlog/BlogModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace MicroBlog
+{
+    public class BlogModule : NancyModule
+    {
+        
+    }
+}
