9dfd6d080818bfcb3c087c68d35619bc0de09bb9
Lee <gould.lee@googlemail.com>
2015-07-09 09:59:52 +0100

    user identity

---
+++ MicroBlog/Models/UserIdentity.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Nancy.Security;
+
+namespace MicroBlog.Models
+{
+    public class UserIdentity : IUserIdentity
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public IEnumerable<string> Claims { get; private set; }
+    }
+}
