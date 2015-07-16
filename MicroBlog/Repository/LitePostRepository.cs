a25d0873baf59bdd6fd7b77ec48a1f8e3f46890f
Lee <gould.lee@googlemail.com>
2015-07-16 12:27:00 +0100

    Messing with a sqlite repo

---
+++ MicroBlog/Repository/LitePostRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data.SQLite;
+
+using Dapper.Contrib.Extensions;
+
+using MicroBlog.Interface;
+using MicroBlog.Models;
+
+namespace MicroBlog.Repository
+{
+    public class LitePostRepository : IPostRepository
+    {
+        private const string Connectionstring = "Data Source=:memory:;Version=3;New=True;";
+
+        public LitePostRepository() 
+        {
+        }
+
+        public Post Get(int id)
+        {
+            using (var conn = new SQLiteConnection(Connectionstring))
+            {
+                conn.Open();
+
+                var p = conn.Get<Post>(id);
+
+                conn.Close();
+
+                return p;
+            }
+        }
+
+        public Post Create(Post post)
+        {
+            using (var conn = new SQLiteConnection(Connectionstring))
+            {
+                conn.Open();
+
+                var postid = conn.Insert(post);
+                post.Id = (int)postid;
+
+                conn.Close();
+            }
+
+            if (post.Id > 0)
+            {
+                return post;
+            }
+
+            return null;
+        }
+
+        public Post Update(Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
