00b6457471bf2997ded553922e476977fe2a5865
Lee <gould.lee@googlemail.com>
2015-07-30 10:24:45 +0100

    file

---
+++ MicroBlog.Web/Scripts/requireconfig.js
@@ -0,0 +1,9 @@
+﻿requirejs.config({
+    "baseUrl": "scripts",
+    "paths": {
+        app: "/app",
+        jquery: "jquery-2.1.4",
+        react: "https://cdnjs.cloudflare.com/ajax/libs/react/0.13.3/react",
+        reacttransformer: "https://cdnjs.cloudflare.com/ajax/libs/react/0.13.3/JSXTransformer"
+    }
+});
