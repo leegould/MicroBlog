35f96351e98aef0cff0b09575a0b94f563918e6e
Lee <gould.lee@googlemail.com>
2015-07-31 10:58:13 +0100

    still more messing with react/require. last try.

--- MicroBlog.Web/Scripts/app.js
+++
@@ -1,9 +0,0 @@
-﻿requirejs.config({
-    "baseUrl": "scripts",
-    "paths": {
-        app: "/app",
-        jquery: "jquery-2.1.4"
-    }
-});
-
-//requirejs('app');
--- MicroBlog.Web/Scripts/requireconfig.js
+++ MicroBlog.Web/Scripts/requireconfig.js
@@ -4,6 +4,9 @@
         app: "/app",
         jquery: "jquery-2.1.4",
         react: "https://cdnjs.cloudflare.com/ajax/libs/react/0.13.3/react",
-        reacttransformer: "https://cdnjs.cloudflare.com/ajax/libs/react/0.13.3/JSXTransformer"
+        JSXTransformer: "JSXTransformer"
+        //reacttransformer: "https://cdnjs.cloudflare.com/ajax/libs/react/0.13.3/JSXTransformer"
     }
 });
+
+requirejs(["jsx!main"]);
