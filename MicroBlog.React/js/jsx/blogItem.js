7687636d86511d16d60f76832f0641e3ac3279dd
Lee <gould.lee@googlemail.com>
2015-09-04 00:17:19 +0100

    files

---
+++ MicroBlog.React/js/jsx/blogItem.js
@@ -0,0 +1,14 @@
+﻿var BlogItem = React.createClass({
+    render: function() {
+        return (
+            <div className="row">
+                <div className="blogheader">
+                    <a href={ 'view.html?id=' + this.props.data.id }>
+                        <h1>{ this.props.data.title }</h1>
+                    </a>
+                </div>
+                <span dangerouslySetInnerHTML={{__html: this.props.data.content}} />
+            </div>
+        );
+    }
+});
