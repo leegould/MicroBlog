ea65a3646ca3f40a0ea9c2f8925777a49f4eea4d
Lee <gould.lee@googlemail.com>
2015-09-04 23:56:20 +0100

    React View page

--- MicroBlog.React/js/jsx/blogItem.js
+++ MicroBlog.React/js/jsx/blogItem.js
@@ -2,6 +2,8 @@
     render: function() {
         return (
             <div className="row">
+                <a className="link" href={ 'edit.html?id=' + this.props.data.id }>Edit</a>
+                <a className="link" href="index.html">Home</a>
                 <div className="blogheader">
                     <a href={ 'view.html?id=' + this.props.data.id }>
                         <h1>{ this.props.data.title }</h1>
