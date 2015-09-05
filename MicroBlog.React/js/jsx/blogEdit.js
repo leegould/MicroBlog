2d079ef9b58e154136d7d1ad4721cab59aa97c20
Lee <gould.lee@googlemail.com>
2015-09-06 00:39:55 +0100

    Some more of the edit form

---
+++ MicroBlog.React/js/jsx/blogEdit.js
@@ -0,0 +1,22 @@
+﻿var BlogEdit = React.createClass({
+    render: function() {
+        return (
+            <form id="blogpostform">
+                <h1>Enter the content for your new blogpost</h1>
+                <div>
+                    <label for="title"></label>
+                    <input type="text" id="title" value="{ this.props.data.Title }" />
+                </div>
+                <div>
+                    <div className="editor">
+                        { this.props.data.content }
+                    </div>
+                </div>
+                <div>
+                    <input type="submit" value="Update" />
+                    <a className="link" href="index.html">Home</a>
+                </div>
+            </form>
+        );
+}
+});
